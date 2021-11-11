588f850c47e9ceb35bd07923db15537c976b5f2a
Ashkan Amouzandeh <33723018+Ashkan999@users.noreply.github.com>
2021-11-10 23:20:44 -0600

    Add moving background

---
+++ Assets/Scripts/BackGround.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGround : MonoBehaviour
+{
+    public float speed;
+
+    public float endPos;
+    public float startPos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+        if (transform.position.x <= endPos) {
+            transform.position = new Vector2(startPos, transform.position.y);
+        }
+    }
+}
