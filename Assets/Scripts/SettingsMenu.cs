d12e1df92a773cb08ba6fcf34937ec400e3a5ccb
Ashkan Amouzandeh <33723018+Ashkan999@users.noreply.github.com>
2021-11-11 16:25:15 -0600

    Add highscore reset

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -1,12 +1,24 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
+    public Slider volumeSlider;
     [SerializeField] private GameObject firstButtonSelected;
 
     void OnEnable()
     {
         EventSystem.current.SetSelectedGameObject(firstButtonSelected);
+    }
+
+    public void AdjustVolume()
+    {
+        // AudioListener.volume = volumeSlider.value;
+    }
+
+    public void ResetHighScore()
+    {
+        PlayerPrefs.DeleteKey("HighScore");
     }
 }
