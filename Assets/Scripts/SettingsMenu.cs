48062086c17b54f4173f0ab89c63bc7478c333d1
Ashkan Amouzandeh <33723018+Ashkan999@users.noreply.github.com>
2021-11-11 15:47:24 -0600

    Add SettingsMenu

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -1,16 +1,12 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
-using TMPro;
 
-public class HighScoreMenu : MonoBehaviour
+public class SettingsMenu : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI highScoreNumber;
     [SerializeField] private GameObject firstButtonSelected;
 
     void OnEnable()
     {
         EventSystem.current.SetSelectedGameObject(firstButtonSelected);
-
-        highScoreNumber.text = PlayerPrefs.GetInt("HighScore", 0).ToString();
     }
 }
